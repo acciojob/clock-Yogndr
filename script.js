391504de0fd38435270049103519b8af7060a19f
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-29 21:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let timerdisplay=document.getElementById("timer");
+
+setInterval(
+function(){
+	let newdate=new Date();
+	timerdisplay.textContent=newdate.toLocaleString()
+},1000)
